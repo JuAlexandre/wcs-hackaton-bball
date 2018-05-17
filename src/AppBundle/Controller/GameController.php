69665b99d0cd74738af9c7406f7f4abf1c292ca7
Jérôme Legrand <jerome.legrand4537@gmail.com>
2018-05-17 15:08:07 +0200

    création du controller

--- src/AppBundle/Controller/GameController.php
+++ src/AppBundle/Controller/GameController.php
@@ -8,17 +8,26 @@
 
 namespace AppBundle\Controller;
 
-use Symfony\Component\HttpKernel\Tests\Controller;
+use AppBundle\Entity\AbstractGame;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
 
 /***
  * Class GameController
  * @package AppBundle\Controller
+ * @Route("admin/games", name="admin_game_list")
+ * @Method("GET")
  */
 class GameController extends Controller
 {
-
     public function adminList()
     {
+        $em = $this->getDoctrine()->getManager();
+        $games = $em->getRepository(AbstractGame::class)->findAll();
 
+        return $this->render('admin/game/index.html.twig', [
+           'games' => $games,
+        ]);
     }
 }
