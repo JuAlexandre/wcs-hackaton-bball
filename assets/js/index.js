6b5a9d84399fd0d03e3fc124dbec134d010dd47a
nooneexpectme <erwann0danguy@gmail.com>
2018-05-17 15:09:17 +0200

    Add navbar burger, menu highlight

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,11 +5,15 @@ import toastr from 'toastr'
 
 // Import ou own JS
 import { displayFlashMessages } from './flash-messages'
+import NavBar from './navbar'
 
 // Export to window object
 window.toastr = toastr;
 
 // When the DOM is ready
 document.addEventListener('DOMContentLoaded', () => {
+    const navBar = new NavBar();
+    navBar.refreshSidebarActiveLocation();
+    navBar.enableNavbarBurgers();
     displayFlashMessages();
 })
